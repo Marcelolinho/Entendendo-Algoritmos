a7f371067022c08ddc23df9ed96bfd9dde6c87d2
Marcelo Laptop <mppozzobon8@gmail.com>
2025-01-22 22:04:54 -0300

    Exercises Chapter 4, part 1

--- recursion.py
+++ recursion.py
@@ -13,3 +13,25 @@ def cumsum(target):
     return target + cumsum(target - 1)
 
 print(cumsum(10))
+
+# Exercício 4.1
+def soma(array):
+    if len(array) < 2:
+        return array[0]
+
+    return array[0] + soma(array[1:])
+
+print(soma([10,20,30,40]))
+
+#Exercício 4.2
+def count_list(array, count = 1):
+    if len(array) == 1:
+        return 1
+    elif len(array) == 0:
+        return 0
+
+    return count + count_list(array[1:], 1)
+
+print(count_list([12, 3, 2, 3, 4, 5]))
+
+#Exercício 4.3
